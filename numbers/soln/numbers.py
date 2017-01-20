7c211f8c9401c535dd2f8342f44613154f11b516
florafeng <sfeng3@ualberta.ca>
2017-01-20 10:39:12 -0700

    Add class notes

--- numbers/soln/numbers.py
+++ numbers/soln/numbers.py
@@ -4,6 +4,7 @@ array = [None] * num_friends
 
 # for each friend, read their numbers..
 for i in range(num_friends):
+    # input().split() is fine
     num_fav = input().strip().split()
     array[i] = num_fav
 
@@ -11,14 +12,17 @@ for i in range(num_friends):
 index = int(input())
 
 # print the favourite numbers of the selected friend
-# count = array[index][0]
 pointer = 1
 end = len(array[index])-1
 while pointer <= (end):
-    if pointer == (end):
+    if pointer == (end): # if it's the end of output, no space!!
         print(array[index][pointer], end="")
     else:
         print(array[index][pointer], end=" ")
     pointer += 1
-    
 print()
+
+# alternate method
+print(" ".join(array[index]))
+          # do   # interater
+list( map( int, array[index]) )
